7aa851590c4a1b4d2bd871c6720f8501b7a8933d
Owen Glantz <oglantz@uci.edu>
2024-03-27 23:28:14 -0700

    Implemented a way to check if all letters of words from the db exist in the given wordset

--- word_processing.py
+++ word_processing.py
@@ -1,9 +1,16 @@
+from collections import Counter
+
+
 
 class WordProcessor():
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, characters):
         self.file_name = file_name
         self.word_list = []
+        self.character_counter = Counter(characters)
+        self.characters = characters
+        self.valid_words = []
+
         self._process_file()
 
     def _process_file(self):
@@ -13,7 +20,21 @@ class WordProcessor():
                 x = i.split("\t")
                 self.word_list.append(x)
     
-    
+    def generate_valid_words(self):
+        for word_and_def in self.word_list:
+            current_word_counter = Counter(word_and_def[0])
+            if(self._valid_word_checker(current_word_counter, self.character_counter)):
+                self.valid_words.append(word_and_def)
 
-x = WordProcessor("wordset.txt")
+    def _valid_word_checker(self, str1:Counter, str2:Counter):
+        for char, count in str1.items():
+            if str2[char] < count:
+                return False
+        return True
+
+
+characters = "AALSEFR"
+x = WordProcessor("wordset.txt", characters)
+x.generate_valid_words()
+print(x.valid_words)
 

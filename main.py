3d668621593da881e76ed9e0193da7e7a2159f75
Owen Glantz <oglantz@uci.edu>
2024-03-29 00:58:46 -0700

    Added a function to calculate pts of each valid word and add them to a dictionary mapping the total pts to the word itself.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import word_processing
+from word_processing import WordProcessor
 
 
 
 def main():
-    inputtedStrings = input("Please enter your scrabble letters, \
-                            plus one from the board")
-    inputtedStrings.strip()
+    inputted_chars = input(
+        "Please enter your scrabble letters, plus one from the board (use * for blank tiles)")
+    inputted_chars.strip()
+    wild_count = inputted_chars.count("*")
+
+    characters = inputted_chars.replace("*", "")
+    print(characters)
+    wp = WordProcessor("wordset.txt", inputted_chars, wild_count)
+    wp.generate_valid_words()
+    print(wp.valid_words)
 
 if __name__ == "__main__":
     main()
--- word_processing.py
+++ word_processing.py
@@ -4,12 +4,27 @@ from collections import Counter
 
 class WordProcessor():
 
-    def __init__(self, file_name, characters):
+    def __init__(self, file_name:str, characters:str, wild_count:int):
         self.file_name = file_name
         self.word_list = []
         self.character_counter = Counter(characters)
         self.characters = characters
-        self.valid_words = []
+        self.valid_words_with_def = {}
+        self.valid_word_with_pts = {}
+        self.wild_count = wild_count
+        self.character_values = {"E": 1, "A": 1,
+                                 "I": 1, "O": 1,
+                                 "N": 1, "R": 1, 
+                                 "T": 1, "L": 1,
+                                 "S": 1, "U": 1,
+                                 "D": 2, "G": 2,
+                                 "B": 3, "C": 3,
+                                 "M": 3, "P": 3,
+                                 "F": 4, "H": 4,
+                                 "V": 4, "W": 4,
+                                 "Y": 4, "K": 5,
+                                 "J": 8, "X": 8,
+                                 "Q": 10, "Z": 10}
 
         self._process_file()
 
@@ -19,22 +34,40 @@ class WordProcessor():
             for i in wordset:
                 x = i.split("\t")
                 self.word_list.append(x)
-    
-    def generate_valid_words(self):
-        for word_and_def in self.word_list:
-            current_word_counter = Counter(word_and_def[0])
-            if(self._valid_word_checker(current_word_counter, self.character_counter)):
-                self.valid_words.append(word_and_def)
 
     def _valid_word_checker(self, str1:Counter, str2:Counter):
         for char, count in str1.items():
             if str2[char] < count:
-                return False
+                needed = count - str2[char]
+            
+                # Check if we have enough wild tiles to compensate
+                if self.wild_count >= needed:
+                    self.wild_count -= needed  # Use the wild tiles
+                else:
+                    return False
         return True
+    
+    def calculate_pts(self):
+        for word in self.valid_words_with_def.keys():
+            temp_chars = list(self.characters)
+            total = 0
+            for char in word:
+                if char in temp_chars:
+                    total += self.character_values[char]
+                    temp_chars.remove(char)
+            self.valid_word_with_pts[word] = total
+
+    def generate_valid_words(self):
+        for word_and_def in self.word_list:
+            current_word_counter = Counter(word_and_def[0])
+            if(self._valid_word_checker(current_word_counter, self.character_counter)):
+                self.valid_words_with_def[word_and_def[0]] = word_and_def[1]
 
 
-characters = "AALSEFR"
-x = WordProcessor("wordset.txt", characters)
+characters = "AHFJD**"
+x = WordProcessor("wordset.txt", characters, 1)
 x.generate_valid_words()
-print(x.valid_words)
+x.calculate_pts()
+print(x.valid_word_with_pts)
+print(x.valid_words_with_def)
 
